d2cde562289eeb4a7fb78440f1e91f98decef008
Andrew Grumet <aegrumet@alum.mit.edu>
2023-11-26 14:53:13 -0800

    Removed http header based authentication from websocket routes. We'll need to find another way.

--- websocket-routes.ts
+++ websocket-routes.ts
@@ -1,15 +1,9 @@
 import { Context, Hono } from "./deps.ts";
 import proxyPodping from "./src/proxy-podping.ts";
-import UmbilicalContext from "./src/umbilical-context.ts";
-import verify from "./src/verify.ts";
 
 const websocket = new Hono();
 
 websocket.get("/podping", (c: Context) => {
-  if (!verify(c as UmbilicalContext)) {
-    c.status(401);
-    return c.text("Unauthorized.");
-  }
   return proxyPodping(c);
 });
 
