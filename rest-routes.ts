d23d5f190abe5e0995621b7d372ecf19cf56a713
Andrew Grumet <aegrumet@alum.mit.edu>
2023-12-24 22:00:26 -0800

    Renamed podping-webpush routes.

--- rest-routes.ts
+++ rest-routes.ts
@@ -2,13 +2,13 @@ import { Hono, cors } from "./deps.ts";
 
 import proxyRoutes from "./src/routes/proxy.ts";
 import searchRoutes from "./src/routes/search.ts";
-import notificationsRoutes from "./src/routes/notifications.ts";
+import podpingWebpushRoutes from "./src/routes/podping-webpush.ts";
 
 const rest = new Hono();
 
 rest.use("/*", cors());
 rest.route("/proxy", proxyRoutes);
 rest.route("/search", searchRoutes);
-rest.route("/notifications", notificationsRoutes);
+rest.route("/podping-webpush", podpingWebpushRoutes);
 
 export default rest;
