c8691a68054b9c257402c3eb9c633bfe0a839c0a
Andrew Grumet <aegrumet@alum.mit.edu>
2023-12-27 10:00:41 -0800

    Updated to latest deno std.

--- dev_deps.ts
+++ dev_deps.ts
@@ -1,8 +1,5 @@
-import { assertEquals } from "https://deno.land/std@0.207.0/assert/mod.ts";
+import { assertEquals } from "https://deno.land/std@0.210.0/assert/mod.ts";
 export { assertEquals };
-
-import * as mf from "https://deno.land/x/mock_fetch@0.3.0/mod.ts";
-export { mf };
 
 import {
   afterEach,
@@ -11,11 +8,10 @@ import {
   it,
   afterAll,
   beforeAll,
-} from "https://deno.land/std@0.206.0/testing/bdd.ts";
-
+} from "https://deno.land/std@0.210.0/testing/bdd.ts";
 export { afterEach, beforeEach, describe, it, afterAll, beforeAll };
 
-export { FakeTime } from "https://deno.land/std@0.207.0/testing/time.ts";
+export { FakeTime } from "https://deno.land/std@0.210.0/testing/time.ts";
 
 import {
   assertSpyCall,
@@ -24,8 +20,11 @@ import {
   stub,
   spy,
   type Stub,
-} from "https://deno.land/std@0.207.0/testing/mock.ts";
+} from "https://deno.land/std@0.210.0/testing/mock.ts";
 export { assertSpyCall, assertSpyCalls, returnsNext, stub, spy, type Stub };
 
 export { EventEmitter } from "https://deno.land/x/event@2.0.1/mod.ts";
 export { expect } from "https://deno.land/x/expect@v0.4.0/mod.ts";
+
+import * as mf from "https://deno.land/x/mock_fetch@0.3.0/mod.ts";
+export { mf };
