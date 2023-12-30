e175236ca6acee065d42e0cc51aa711824f29c12
Andrew Grumet <aegrumet@alum.mit.edu>
2023-12-30 12:17:47 -0800

    Revert "Testing if we need to put cors middleware at leaf level routes."
    This reverts commit 6790febe25cb5293d295491602e5cade5e6b57c4.

--- rest-routes-server.ts
+++ rest-routes-server.ts
@@ -1,9 +1,10 @@
-import { Hono } from "./deps.ts";
+import { Hono, cors } from "./deps.ts";
 
 import podpingWebpushRoutes from "./src/routes/podping-webpush.ts";
 
 const restServer = new Hono();
 
+restServer.use("*", cors());
 restServer.route("/podping-webpush", podpingWebpushRoutes);
 
 export default restServer;
